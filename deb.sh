4f20f9a1b367be016e3f6c9352482e960a9456bc
Simon Byrne <simonbyrne@gmail.com>
2017-03-14 15:05:23 +0000

    minor changes, hopefully platform independent

--- deb.sh
+++ deb.sh
@@ -1,4 +1,5 @@
- #!/bin/sh
+ #!/bin/bash
+set -e
 
 JULIA_VER=0.5.1
 
@@ -6,32 +7,31 @@ JULIA_VER=0.5.1
 
 wget https://github.com/JuliaLang/julia/releases/download/v$JULIA_VER/julia-$JULIA_VER.tar.gz
 mkdir julia
-tar -zxvf julia-0.5.1.tar.gz -C julia --strip-components=1
+tar -zxvf julia-$JULIA_VER.tar.gz -C julia --strip-components=1
 export OPENBLAS_NUM_THREADS=1
 
 cp Make.user julia
 
 cd julia && make -j4 binary-dist
 
-JULIA_COMMIT=`ls julia*-Linux-arm.tar.gz | cut -b 7-16`
+JULIA_COMMIT=`ls julia*-Linux-*.tar.gz | cut -b 7-16`
 #JULIA_VER=`./bin/julia -version | cut -b 15-19`
 
 
-rm -fr julia-$JULIA_VER julia_* && \
-    tar zxf julia-$JULIA_VER-Linux-arm.tar.gz && \
-    cd julia-$JULIA_VER && \
-    mv LICENSE.md share/doc/julia/LICENSE.md && \
-    rm -f lib/julia/libpcre2-posix.so* && \
-    rm -f lib/julia/libstdc++.so* && \
-    rm -f bin/*-debug* lib/*-debug* lib/julia/*-debug* && \
-    rm -fr libexec && \
-    rm -f lib/julia/libccalltest* && \
-    gcc -shared /usr/lib/arm-linux-gnueabihf/libsuitesparseconfig.a -o lib/julia/libsuitesparseconfig.so && \
-    tar cf ../julia-$JULIA_VER.tar * && \
-    cd .. && \
-    rm -fr julia-$JULIA_VER && \
-    fakeroot alien -d --generate julia-$JULIA_VER.tar && \
-    cp -f ../control julia-$JULIA_VER/debian && \
-    cd julia-$JULIA_VER && \
-    debuild --no-lintian
-
+rm -fr julia-$JULIA_VER julia_*
+tar zxf julia-$JULIA_VER-Linux-*.tar.gz
+cd julia-$JULIA_VER
+mv LICENSE.md share/doc/julia/LICENSE.md
+rm -f lib/julia/libpcre2-posix.so*
+rm -f lib/julia/libstdc++.so*
+rm -f bin/*-debug* lib/*-debug* lib/julia/*-debug*
+rm -fr libexec
+rm -f lib/julia/libccalltest*
+gcc -shared /usr/lib/*-linux-*/libsuitesparseconfig.a -o lib/julia/libsuitesparseconfig.so
+tar cf ../julia-$JULIA_VER.tar *
+cd ..
+rm -fr julia-$JULIA_VER
+fakeroot alien -d --generate julia-$JULIA_VER.tar
+cp -f ../control julia-$JULIA_VER/debian
+cd julia-$JULIA_VER
+debuild --no-lintian
